516acee93991866efc61c605fe6e166a32135c2a
leeseulgi0208 <106726904+leeseulgi0208@users.noreply.github.com>
2025-05-26 00:28:36 +0900

    feat/#7 - RoutineModel, RoutineView, RoutineViewModel 생성

---
+++ routina-mobile-ios/routina-mobile-ios/App/Plan/Routine/RoutineModel.swift
@@ -0,0 +1,13 @@
+//
+//  RoutineModel.swift
+//  routina-mobile-ios
+//
+//  Created by 이슬기 on 5/26/25.
+//
+
+import Foundation
+
+struct RoutineItem {
+    let icon: String
+    let title: String
+}
