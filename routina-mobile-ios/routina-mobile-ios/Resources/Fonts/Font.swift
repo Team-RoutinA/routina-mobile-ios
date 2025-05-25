724dc7e735a910526894ae4ba0d8605b9c604126
hamgui-2022 <ohjhlee14@gachon.ac.kr>
2025-05-25 14:29:31 +0900

    프로젝트 생성, 폰트 추가

---
+++ routina-mobile-ios/routina-mobile-ios/Resources/Fonts/Font.swift
@@ -0,0 +1,54 @@
+//
+//  Font.swift
+//  routina-mobile-ios
+//
+//  Created by 이재혁 on 5/25/25.
+//
+
+import Foundation
+import SwiftUI
+
+extension Font {
+    enum Pretend {
+        case black
+        case bold
+        case extraBold
+        case extraLight
+        case light
+        case medium
+        case regular
+        case semibold
+        case thin
+
+        var value: String {
+            switch self {
+            case .black:
+                return "Pretendard-Black"
+            case .bold:
+                return "Pretendard-Bold"
+            case .extraBold:
+                return "Pretendard-ExtraBold"
+            case .extraLight:
+                return "Pretendard-ExtraLight"
+            case .light:
+                return "Pretendard-Light"
+            case .medium:
+                return "Pretendard-Medium"
+            case .regular:
+                return "Pretendard-Regular"
+            case .semibold:
+                return "Pretendard-SemiBold"
+            case .thin:
+                return "Pretendard-Thin"
+            }
+        }
+    }
+    
+    static func pretend(type: Pretend, size: CGFloat) -> Font {
+        return .custom(type.value, size: size)
+    }
+    
+    static var PretendardBold30: Font {
+        return .pretend(type: .bold, size: 30)
+    }
+}
---
+++ routina-mobile-ios/routina-mobile-ios/routina_mobile_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  routina_mobile_iosApp.swift
+//  routina-mobile-ios
+//
+//  Created by 이재혁 on 5/25/25.
+//
+
+import SwiftUI
+
+@main
+struct routina_mobile_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
