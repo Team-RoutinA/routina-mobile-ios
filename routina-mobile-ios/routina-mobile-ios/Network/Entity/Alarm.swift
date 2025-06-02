25d29da2467838ee1316f0d1c602a26734f32dcf
leeseulgi0208 <106726904+leeseulgi0208@users.noreply.github.com>
2025-06-02 20:12:21 +0900

    [network] #36 - alarm entity 생성

---
+++ routina-mobile-ios/routina-mobile-ios/Network/Entity/Alarm.swift
@@ -0,0 +1,20 @@
+//
+//  Alarm.swift
+//  routina-mobile-ios
+//
+//  Created by 이슬기 on 6/2/25.
+//
+
+import Foundation
+
+struct AlarmCreateRequest: Encodable {
+    let time: String
+    let status: String
+    let sound_volum: Double
+    let repeat_days: [Int]?
+    struct Routine: Encodable {
+        let routine_id: String
+        let order: Int
+    }
+    let routines: [Routine]
+}
